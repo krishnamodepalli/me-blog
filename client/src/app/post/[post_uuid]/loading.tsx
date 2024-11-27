0c25369547734f1907dfb66022b126f920e4749c
Krishna Modepalli <90754999+krishnamodepalli@users.noreply.github.com>
2024-11-27 14:41:11 +0530

    feat(ui): add loading page while loading blog-post
    what's changed:
    - Add a new loading page while rendering the blog-post in the
      client-side

---
+++ client/src/app/post/[post_uuid]/loading.tsx
@@ -0,0 +1,29 @@
+const Loading = () => {
+  return (
+    <div>
+      <p className="mb-20 w-10/12 animate-pulse rounded-lg bg-bg3 p-10"></p>
+      <div className="my-14">
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 w-11/12 animate-pulse rounded-md bg-bg3 p-3"></p>
+      </div>
+      <div className="my-14">
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 w-11/12 animate-pulse rounded-md bg-bg3 p-3"></p>
+      </div>
+      <div className="my-14">
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 w-11/12 animate-pulse rounded-md bg-bg3 p-3"></p>
+      </div>
+      <div className="my-14">
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 animate-pulse rounded-md bg-bg3 p-3"></p>
+        <p className="mb-2 w-11/12 animate-pulse rounded-md bg-bg3 p-3"></p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
