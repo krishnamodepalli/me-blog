e6b7461db05a45ecb710fb9688a7847f72dc80d2
Krishna Modepalli <90754999+krishnamodepalli@users.noreply.github.com>
2024-12-03 14:05:52 +0530

    perf(client/draftings): `saveToLocal` function optimised
    what's changed:
    - The `saveToLocal` function in the `/edit` page is now more performant
      by using the ref for the state holding the `editingData`

--- client/src/app/root/draft/[post_uuid]/edit/page.tsx
+++ client/src/app/root/draft/[post_uuid]/edit/page.tsx
@@ -5,71 +5,60 @@ import { toast } from "react-toastify";
 import Cookies from "js-cookie";
 
 import { jetbrains } from "@/app/_fonts";
-import getDraft from "@/app/_utils/getDraft";
-import { IDraft } from "@/app/_types";
+import {
+  getDraft as getDraftFromLocal,
+  updateDraft,
+} from "@/app/_utils/storage";
+import { IDraftDetails } from "@/app/_types";
 
 const Page = ({ params }: { params: { post_uuid: string } }) => {
   const { post_uuid } = params;
   const token = Cookies.get("token") as string;
 
-  const [editingData, setEditingData] = useState({
+  const [editingData, setEditingData] = useState<IDraftDetails>({
     title: "",
     content: "",
   });
+  const editingDataRef = useRef<IDraftDetails>(editingData);
   const lastFetchedRef = useRef<string>("");
 
   useEffect(() => {
-    const draftData = JSON.parse(
-      localStorage.getItem(`draft:${post_uuid}`) as string,
-    );
-    let draft: IDraft;
-    if (draftData === null) {
-      getDraft(token, post_uuid).then((res) => {
-        setEditingData({ ...res });
-      });
-    } else {
-      draft = draftData.draft;
-      setEditingData({
-        title: draft.title,
-        content: draft.content,
-      });
-    }
+    const draft = getDraftFromLocal(post_uuid);
+    if (!draft) return;
+    setEditingData(draft.draft);
 
-    lastFetchedRef.current = draftData.lastFetched;
+    lastFetchedRef.current = draft.fetchedAt;
   }, [token, post_uuid]);
 
+  /**\
+   * This ref is used to save the editing data for every 3 seconds of inactivity
+   */
   const timeoutRef = useRef<NodeJS.Timeout>();
 
+  /**
+   * Save the editing data into localStorage
+   */
   const saveToLocal = useCallback(() => {
-    localStorage.setItem(
-      `draft:${post_uuid}`,
-      JSON.stringify({
-        draft: editingData,
-        lastUpdated: Date.now().toString(),
-        lastFetched: lastFetchedRef.current,
-      }),
-    );
+    const data = editingDataRef.current;
+    updateDraft(post_uuid, data);
     toast.success("Saved to local.", {
       theme: "colored",
       autoClose: 1500,
     });
-  }, [editingData, post_uuid]);
+  }, [post_uuid]);
 
-  // effect to save for every 5 seconds after any change
+  // effect to save for every 3 seconds after any change
   useEffect(() => {
     // Clear the existing timeout if there is a change
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
     // Set a new timeout for auto-saving
     timeoutRef.current = setTimeout(() => {
       saveToLocal();
     }, 3000);
 
     return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
     };
   }, [saveToLocal]);
 
@@ -89,7 +78,7 @@ const Page = ({ params }: { params: { post_uuid: string } }) => {
       <div id="title" className="mb-8">
         <div className="relative border-l-2 border-t2 p-2 pl-4 text-6xl font-bold">
           <textarea
-            className="z-1 block h-16 w-full resize-none overflow-hidden bg-transparent outline-none placeholder:text-dim"
+            className="z-1 placeholder:text-t4 block h-16 w-full resize-none overflow-hidden bg-transparent outline-none"
             value={editingData.title}
             placeholder="Your title goes here..."
             onChange={(e) => {
@@ -105,6 +94,7 @@ const Page = ({ params }: { params: { post_uuid: string } }) => {
       <div className="mt-20 rounded-lg bg-bg2" id="content">
         <textarea
           value={editingData.content}
+          placeholder="Start your story here..."
           onChange={(e) => {
             setEditingData((prev) => ({
               title: prev.title,
